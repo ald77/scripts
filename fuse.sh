99bd5c132281d280d9689171154fb70244ddd80b
Adam Dishaw <aldishaw@gmail.com>
2015-12-08 12:02:49 +0100

    Ensure mnt path exists and build ROOT in debug mode

--- fuse.sh
+++ fuse.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+mkdir -p "/Users/adam/mnt/$1"
 sshfs "$1:/" "/Users/adam/mnt/$1" -o IdentityFile=/Users/adam/.ssh/id_rsa -o uid=501 -o gid=20 -o follow_symlinks -o umask=0 -o reconnect -o allow_other
--- upROOT.sh
+++ upROOT.sh
@@ -4,7 +4,7 @@ mydir=$(pwd)
 
 cd $ROOTSYS
 sudo git pull
-sudo ./configure --all
+sudo ./configure --all --build=debug --cxxflags="-g -pg -O2" --cflags="-g -pg -O2"
 sudo nice -n 19 make -k
 source bin/thisroot.sh
 
