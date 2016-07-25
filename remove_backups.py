545827f1746b7b041a7b104439a895776734c531
Adam Dishaw <aldishaw@gmail.com>
2016-07-25 13:10:14 -0700

    Handle file permission errors

--- remove_backups.py
+++ remove_backups.py
@@ -1,21 +1,30 @@
 #! /usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
 import os
 
 def FullPath(path):
-    return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+    return os.path.abspath(os.path.expanduser(path))
 
 def RemoveBackups(file_dir):
     full_path = FullPath(file_dir)
     if os.path.isdir(full_path):
-        contents = os.listdir(full_path)
+        contents = []
+        try:
+            contents = os.listdir(full_path)
+        except OSError as err:
+            print("Cannot access directory "+full_path+" (OSError "+str(err.errno)+": "+err.strerror+")")
         for fd in contents:
             RemoveBackups(os.path.join(full_path, fd))
     elif os.path.isfile(full_path):
         base_name = os.path.basename(full_path)
         if base_name.endswith('~') or (base_name.startswith('#') and base_name.endswith('#')):
-            os.remove(full_path)
+            try:
+                os.remove(full_path)
+            except OSError as err:
+                print("Cannot remove file "+full_path+" (OSError "+str(err.errno)+": "+err.strerror+")")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Removes all emacs backup files from given directories",
