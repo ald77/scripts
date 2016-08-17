65047eead789cdc77c8463d08eb7b389ee6fde95
Adam Dishaw <aldishaw@gmail.com>
2016-08-16 21:45:34 -0700

    Look for scratch directory when checking compute nodes

--- update_skipped_nodes.sh
+++ update_skipped_nodes.sh
@@ -4,6 +4,6 @@ temp=$(mktemp ~/tmp.XXXXXXXXXXXX)
 chmod 644 $temp
 for i in `seq 0 36`
 do
-    ssh "cms$i" "ls /net/cms27/cms27r0 &> /dev/null && [[ \$HOSTNAME == compute* ]] && echo \"Keeping cms$i (\$HOSTNAME, \`cat /etc/redhat-release\`)\" || (echo cms$i >> $temp && echo \"Rejecting cms$i (\$HOSTNAME, \`cat /etc/redhat-release\`)\")" || (echo "cms$i" >> $temp && echo "Rejecting cms$i (could not connect)")
+    ssh "cms$i" "ls /net/cms27/cms27r0 &> /dev/null && ls /scratch/babymaker &> /dev/null &&[[ \$HOSTNAME == compute* ]] && echo \"Keeping cms$i (\$HOSTNAME, \`cat /etc/redhat-release\`)\" || (echo cms$i >> $temp && echo \"Rejecting cms$i (\$HOSTNAME, \`cat /etc/redhat-release\`)\")" || (echo "cms$i" >> $temp && echo "Rejecting cms$i (could not connect)")
 done
 mv $temp $JOBS/skippednodes.list
